56844ddf8f5f9b797690d3a2f8fa9e424cdd3426
Braian Barrientos <braianbts@gmail.com>
2023-11-20 20:26:45 -0300

    feat:new images

--- src/utils/projectsDetails.jsx
+++ src/utils/projectsDetails.jsx
@@ -3,7 +3,7 @@ const projectsDetails = [
     title: "First Plug Argentina",
     description:
       "Desarrollo de plataforma web donde el foco principal es la gestión, adquisición y control de inventario, simplificando el onboarding y equipamiento para trabajadores remotos.",
-    image: "/firstplug.jpeg",
+    image: "/optimized_firstplug.jpg",
     tech: "Express + MongoDB + Mongoose + NextJs + Tailwind + MST (Mobx State Tree) + Typescript",
     client: "https://github.com/sebikap-gm2/First-Plug-Platform-Client",
     api: "https://github.com/sebikap-gm2/First-Plug-Platform-Api",
@@ -12,7 +12,7 @@ const projectsDetails = [
     title: "BOX",
     description:
       "Aplicación web (mobile first) para reparto de productos/paquetería con geolocalización y seguimiento en tiempo real. Posee perfiles de usuario, administrador y repartidor.",
-    image: "/box.jpg",
+    image: "/optimized_box.jpg",
     tech: "Express + MongoDB + Mongoose + NextJs + Tailwind + MST (Mobx State Tree) + Typescript + Firebase + AWS + JWT",
     client: "https://github.com/BrianBts/box-client",
     api: "https://github.com/Francisco-Villanueva/box-api",
@@ -21,7 +21,7 @@ const projectsDetails = [
     title: "The movie Database",
     description:
       "Aplicacion web para buscar peliculas y series, consumiendo la API de TMDB. Posee registro de usuarios y login. ",
-    image: "/tmdb.jpg",
+    image: "/optimized_tmdb.jpg",
     tech: "Vite, React, Node, Express, PostgreSQL, Sequelize, Tailwind",
     client: "https://github.com/BrianBts/TMDB",
     api: "https://github.com/BrianBts/26-checkpoint-TMDB/tree/main/api",
@@ -29,7 +29,7 @@ const projectsDetails = [
   {
     title: "Behance",
     description: "Perfil de Behance con proyectos de Diseño Gráfico",
-    image: "/behance.jpg",
+    image: "/optimized_behance.jpg",
     tech: "Adobe Photoshop, Adobe Ilustrator, Adobe Lightroom, Adobe Premiere.",
     client: "https://www.behance.net/brianbts",
     api: "https://www.behance.net/brianbts",
@@ -38,7 +38,7 @@ const projectsDetails = [
     title: "Rincón Matero Eccomerce",
     description:
       "Eccomerce de venta de productos para el mate. Posee registro de usuarios y login, perfil de usuario, carrito de compras, checkout, pagos con Mercado Pago, panel de administrador.",
-    image: "/rincon-matero.jpg",
+    image: "/optimized_rincon_matero.jpg",
     tech: "Vite, React, Redux, Sass, Node, Express, PostgreSQL, Sequelize, JWT",
     client: "https://github.com/BrianBts/rincon-matero-front",
     api: "https://github.com/BrianBts/rincon-matero-back",
