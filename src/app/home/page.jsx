e0929cf9f483d98027ee15f374deb076c78d3338
Braian Barrientos <braianbts@gmail.com>
2023-11-24 16:29:34 -0300

    feat(client): new feat

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -6,21 +6,18 @@ import Projects from "../../components/Projects";
 import Contact from "../../components/Contact";
 import Start from "../../components/Start";
 import Footer from "@/components/Footer";
+import TimeLine from "@/components/TimeLine";
 
 const Page = () => {
   return (
     <div className="relative bg-backgroundColor dark:bg-backGrey">
       <Navbar />
       <Start />
-
       <About />
-
+      <TimeLine/>
       <Skills />
-
       <Projects />
-
       <Contact />
-
       <Footer />
     </div>
   );
